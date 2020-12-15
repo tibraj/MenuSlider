3af0f6db6f00af3b6180e90646c1601f1cf8a877
tibraj <tibraj94@gmail.com>
2020-12-15 15:13:17 -0500

    event for toggling navbar

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
+
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'))
