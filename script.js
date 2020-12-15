4a0906774f0cf44dabb44fca0a3f871a86d5b02c
tibraj <tibraj94@gmail.com>
2020-12-15 15:21:29 -0500

    event for closing modal when clicking away

--- script.js
+++ script.js
@@ -8,3 +8,5 @@ toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'
 open.addEventListener('click', () => modal.classList.add('show-modal'));
 
 close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
